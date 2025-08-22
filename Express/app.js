575036f0de4d7147a88f6b3a33f29203a2386c4b
Yohanes Debebe <yopeman318@gmail.com>
2025-08-22 14:57:31 +0300

    Enable helmet middleware for enhanced security and add CORS headers for API requests

--- Express/app.js
+++ Express/app.js
@@ -20,9 +20,15 @@ const __dirname = dirname(__filename);
 
 const app = express();
 app.use(cors());
-// app.use(helmet());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
 
 app.use(request_parser);
 
