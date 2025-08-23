de36456dc5af1d618b55173152e46b4e65270e22
Yohanes Debebe <yopeman318@gmail.com>
2025-08-23 12:48:39 +0300

    Refactor code structure for improved readability and maintainability

--- Express/app.js
+++ Express/app.js
@@ -27,7 +27,6 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Content-Security-Policy", "script-src 'self' https://vercel.live");
     next();
 });
 
