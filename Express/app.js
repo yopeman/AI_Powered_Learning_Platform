5aa1d547f069e1ca593b8d48fc0f1d39cd4ebafa
Yohanes Debebe <yopeman318@gmail.com>
2025-08-23 12:44:36 +0300

    Add Content-Security-Policy header to enhance security

--- Express/app.js
+++ Express/app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Content-Security-Policy", "script-src 'self' https://vercel.live");
     next();
 });
 
